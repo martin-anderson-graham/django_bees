8cccff06a90841beb67ed77ee8d8343c7caed7e3
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2023-02-10 16:07:36 -0600

    Update view to pull data from the aiven db

--- django_bees/games/views.py
+++ django_bees/games/views.py
@@ -1,8 +1,18 @@
 from django.http import JsonResponse
-
+from .models import Game
 # Create your views here.
+
+
 def get_all_games(request):
-    data = {
-            'some': 'test data'
-            }
-    return JsonResponse(data)
+    qs = list(Game.objects.all())
+    all_games = []
+    for query in qs:
+        all_games.append({
+            "id": query.id,
+            "date_played": query.date_played,
+            "opposing_team_name": query.opposing_team_name,
+            "opposing_score": query.opposing_score,
+            "brentford_score": query.brentford_score,
+            "brentford_victory": query.brentford_victory
+        })
+    return JsonResponse({"games": all_games})
